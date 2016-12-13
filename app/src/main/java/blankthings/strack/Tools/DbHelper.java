ff179e09ea115d587b44e00a088e54b0732685f8
Iosif Vilcea <iosifvilcea@gmail.com>
2016-12-12 22:19:46 -0600

    basic dbhelper.

---
+++ app/src/main/java/blankthings/strack/Tools/DbHelper.java
@@ -0,0 +1,30 @@
+package blankthings.strack.Tools;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * DbHelper
+ *   Stores data locally.
+ *
+ * Created by iosifvilcea on 12/11/16.
+ */
+
+public class DbHelper extends SQLiteOpenHelper {
+
+    public DbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+}
